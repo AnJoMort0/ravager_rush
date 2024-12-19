7dfa994036800299135bb7a1c2364d3c595fbf78
AnJoMort0 <andrejsfonseca@gmail.com>
2024-12-19 01:41:51 +0100

    obstacls have been added

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ const SCREEN_WIDTH  = 256 * SCALE;
 const SCREEN_HEIGHT = 336 * SCALE;
 const FIX           = (16 * SCALE) / 2; //this is to correct position for elements that need to be anchored to the center
 
+const Z_TOP = 10;
+
 function at(square){return (GRID_SIZE * square)}; //function to ease the calculations of squares
 
-let last_pos        = [at(7), at(19)];
+let last_pos        = [at(8), at(19)];
 
 kaplay({
     width: SCREEN_WIDTH,
@@ -22,9 +24,11 @@ kaplay({
 
 //Assets
 loadRoot('assets/');
-loadSprite("board"  , "temp/board.png");
-loadSprite("player" , "player.png");
-loadSprite("blocker", "blocker.png");
+loadSprite("board"          , "temp/board.png");
+loadSprite("player"         , "player.png");
+loadSprite("blank16x16"     , "blank16x16.png");
+loadSprite("blocker"        , "blocker.png");
+loadSprite("blocker_double" , "blocker_double.png");
 
 scene("main", () => {
     // Add the board background
@@ -42,39 +46,9 @@ scene("main", () => {
         area(),
         "player",
     ]);
-    player.onCollide("blocker", () => { //setting the player back to it's previous position when intercepting a blocker --> not letting it move
+    player.onCollide("blocker", () => { //sets the player back to it's previous position when intercepting a blocker --> not letting them move
         player.pos = vec2(last_pos);
     })
-
-    function addblocker (coorX, coorY, Z){
-        add([
-            sprite("blocker"),
-            scale(SCALE),
-            anchor("center"),
-            pos(at(coorX) + FIX, at(coorY) + FIX),
-            z(Z),
-            area(scale(0.7)),
-            "blocker",
-        ]);
-    }
-
-    // Add walls
-    const numRows = SCREEN_HEIGHT / (GRID_SIZE);
-    for (let i = 0; i < numRows; i++) { 
-        addblocker(0  , i , 0); //left wall
-        addblocker(15 , i , 0); //right wall
-    }
-    const numCols = SCREEN_WIDTH / (GRID_SIZE);
-    for (let i = 0; i < numCols; i++) {
-        addblocker(i, 3, 0); //top line
-        addblocker(3, 4, 0);
-        addblocker(6, 4, 0);
-        addblocker(9, 4, 0);
-        addblocker(12, 4, 0);
-        addblocker(i, 20, 0); //bottom line
-    }
-
-    // Movement logic for the player
     onKeyPress(["up", "w"], () => {
         last_pos = [player.pos.x, player.pos.y];
         player.pos.y -= GRID_SIZE;
@@ -91,6 +65,71 @@ scene("main", () => {
         last_pos = [player.pos.x, player.pos.y];
         player.pos.x += GRID_SIZE;
     });
+
+    //Add game elements
+    function addBlocker (coorX, coorY, Z, isDouble){
+        if (!isDouble) {
+            add([
+                sprite("blocker"),
+                scale(SCALE),
+                anchor("center"),
+                pos(at(coorX) + FIX, at(coorY) + FIX),
+                z(Z),
+                area(scale(0.7)),
+                "blocker",
+            ]);
+        } else {
+            add([
+                sprite("blocker_double"),
+                scale(SCALE),
+                anchor("center"),
+                pos(at(coorX) + FIX * 2, at(coorY) + FIX),
+                z(Z),
+                area(scale(0.7)),
+                "blocker",
+            ]);
+        }
+    }
+
+        /////////THESE WALLS HAVE TO BE TEMPORARY BECAUSE THEY USE TOO MUCH PERFORMANCE. THEY NEED TO BE A SINGLE ELEMENT AND NOT A WHOLE BUNCH OF WALLS (execpt maybe between the frog doors)
+        // Add walls
+        const numRows = SCREEN_HEIGHT / (GRID_SIZE);
+        for (let i = 0; i < numRows; i++) { 
+            addBlocker(0  , i , 0); //left wall
+            addBlocker(15 , i , 0); //right wall
+        }
+        const numCols = SCREEN_WIDTH / (GRID_SIZE);
+        for (let i = 0; i < numCols; i++) {
+            addBlocker(i, 3, 0); //top line
+            addBlocker(3, 4, 0); //top line
+            addBlocker(6, 4, 0); //top line
+            addBlocker(9, 4, 0); //top line
+            addBlocker(12, 4, 0); //top line
+            addBlocker(i, 20, 0); //bottom line
+        }
+
+        // Add blocker obstacles
+        let blockerCoords = [
+            [18, [2, 3, 6, 10, 11, 14]],
+            [16, [3, 6, 7, 10, 13, 14]],
+            [14, [1, 2, 5, 8, 9, 13]],
+        ];
+        for (let i = 0; i < blockerCoords.length; i++) {
+            const YCoord = blockerCoords[i][0];
+            const XCoord = blockerCoords[i][1];
+        
+            for (let j = 0; j < XCoord.length; j++) {
+                const current   = XCoord[j];
+                const next      = XCoord[j + 1];
+        
+                if (next === current + 1) { // Check if there are consecutive blockers
+                    addBlocker(current, YCoord, Z_TOP, true);
+                    j++;
+                } else {
+                    addBlocker(current, YCoord, Z_TOP, false);
+                }
+            }
+        }              
 });
 
 go("main");
